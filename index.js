ccb1b6e114c8a95680e9a3e05dc1f00b89e5770c
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-12-29 20:33:54 +0200

    saas

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const { Client } = require("pg");
+const client = new Client();
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
+
+  client.query("SELECT * FROM customers").then((response) => {
+    res.send(response)
+    res.
+  })
   res.send("It is working what dssdsd!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  client.connect().then(() => {
+    console.log("connected to database");
+  });
 });
